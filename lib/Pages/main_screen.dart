b584fb0b500e1d564b3b0bdf5bfc59e03e484498
Can Gönülaçar <cgonulacar5@gmail.com>
2024-01-21 23:27:20 +0300

    Update main_screen.dart

--- lib/Pages/main_screen.dart
+++ lib/Pages/main_screen.dart
@@ -1,10 +1,14 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-   const HomePage({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return Container();
-   }
- }
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("HomePage"),
+      ),
+    );
+  }
+}
